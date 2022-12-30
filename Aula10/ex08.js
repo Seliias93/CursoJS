9c253232d768679d32354ce61565adc2205e6cb7
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2022-12-30 14:20:37 -0300

    .

---
+++ Aula10/ex08.js
@@ -0,0 +1,7 @@
+var vel = 12
+
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if(vel > 60) { //Condiçao simple!
+    console.log(`voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`digija sempre  usando cinto de segurança`)
